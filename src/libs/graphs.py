52507d4e6ee3967c26b6b7c066cda9cb23d0a6a4
Alessio Ganzarolli <ganzarolli.alessio@gmail.com>
2023-10-26 10:09:25 +0200

    add general graph

--- src/libs/graphs.py
+++ src/libs/graphs.py
@@ -1,13 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# from formulas import Formulas
+
 class Graphs():
     def __init__(self):
         pass
-    
-    def getGraph(self):
-        x = np.linspace(0.1, 2 * np.pi, 41)
-        y = np.sin(x)
-
-        plt.stem(x, y)
-        plt.show()
