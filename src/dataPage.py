3f5dd25d68171cff729ea3519ffaa529e7f964ff
ThomasCantuti <96134266+ThomasCantuti@users.noreply.github.com>
2023-11-21 00:07:30 +0100

    add image pkt try to modify position to img pkt

--- src/dataPage.py
+++ src/dataPage.py
@@ -8,6 +8,7 @@ from kivy.properties import Clock
 from kivy.metrics import dp
 from kivy.uix.image import AsyncImage
 from kivy.animation import Animation
+from kivy.uix.image import AsyncImage
 
 Builder.load_string("""
 <DataPage>:
@@ -47,29 +48,34 @@ Builder.load_string("""
         # parte inferiore
         BoxLayout:
             ScrollView:
+                id: system
                 do_scroll_y: True
-                Button:
-                    size_hint: None, None
-                    size: 100, 50
-                    text: "ciao"
-                    on_press: root.on_click()
 
 """)
 
 class DataPage(Screen):
     bg_color = Styles.primary_color
     secondary_color = Styles.secondary_color
+    pkt_img = AsyncImage()
+    pkt_img.source = "img/pkt.png"
+    #pkt_img.size_hint = (None, None)
+    #pkt_img.size = (dp(50), dp(50))
+    pkt_img.size_hint = (0.2, 0.2)
+    
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.rect_size = dp(50)
         self.pipe_size = dp(50)
+        #self.pkt_size = dp(50)
         with self.canvas:
             Color(0,1,0,1)
             self.pipe = Line(points = [self.x, self.center_y, self.right, self.center_y], width = 3)
             #Line(circle = (400,200,80), width = 2)
             #Line(rectangle = (700,500,150,100), width = 2)
             #self.rect = Rectangle(pos = self.center, size = (150, 100))
+            self.pkt_img.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+            self.ids.system.add_widget(self.pkt_img)
         #Clock.schedule_interval(self.update, 1)
     
     def on_click (self):
@@ -91,6 +97,8 @@ class DataPage(Screen):
                             self.center_y - self.pipe_size / 2,
                             self.right / 2, 
                             self.center_y - self.pipe_size / 2)
+        #self.pkt_img.pos = (self.center_x - self.pkt_img.size[0] / 2,
+        #                    self.center_y - self.pkt_img.size[1] / 2)
     
     def update (self, dt):
         #x, y = self.rect.pos
